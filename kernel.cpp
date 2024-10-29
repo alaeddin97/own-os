c19caa18575ab92569cd0a1e54be0949aa4e0d46
alaeddin97 <alagarraouiala@gmail.com>
2024-10-29 16:15:49 -0400

    initialize cpp static & global variables

--- kernel.cpp
+++ kernel.cpp
@@ -6,6 +6,15 @@ void printf(char *str) {
         videoMemory[i] = (0x0F << 8) | str[i];
 }
 
+typedef void (*constructor) ();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+
+extern "C" void callConstructors() {
+    for(constructor* i = &start_ctors; i!= &end_ctors; i++)
+        (*i)();
+};
+
 extern "C" void kernelMain(void *multiboot_structure, unsigned int magicnumber) {
     printf("Hello world! - - - this is good");
 
